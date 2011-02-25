693d250acc2cde6b5d65a2388d5d045c25d27d76
Zachary Scott <zachary.s.scott@gmail.com>
2011-02-25 11:16:40 -0500

    fork me to repo url

--- app.rb
+++ app.rb
@@ -66,7 +66,7 @@ __END__
       <%= yield %>
     </div> 
   
-    <a href="http://github.com/sinatra">
+    <a href="http://github.com/sinatra/sinatra-book-contrib">
       <img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" />
     </a>
   </body>
