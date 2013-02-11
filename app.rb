d66800aa4a2d76a0475ae2d2187ed20c08b4b4e7
Kashyap <kashyap.kmbc@gmail.com>
2013-02-11 17:47:13 +0530

    Revert "Added Slim gem"
    This reverts commit 4ff1f9f8325c7dbcea706f1b145e52c254b4b258.
    
    reverting this commit

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 require 'rdiscount'
+require 'erb'
 require 'sass'
 require 'json'
 require 'open-uri'
-require 'slim'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 configure :production do
@@ -19,7 +19,7 @@ configure :production do
   end
 end
 
-set :markdown, :layout_engine => :slim
+set :markdown, :layout_engine => :erb
 set :views, File.dirname(__FILE__)
 set :ignored_dirs, %w[tmp log config public bin]
 
@@ -31,12 +31,10 @@ before do
 end
 
 get '/' do
-  begin
-    open("https://api.github.com/repos/sinatra/sinatra-recipes/contributors") do |api|
-      @contributors = JSON.parse(api.read)
-    end 
-  rescue SocketError => e
-  end
+  open("https://api.github.com/repos/sinatra/sinatra-recipes/contributors") { |api|
+    @contributors = JSON.parse(api.read)
+  }
+
   markdown :README
 end
 
@@ -59,78 +57,100 @@ end
 __END__
 
 @@ layout
-doctype 5
-html
-  head
-    meta charset="utf-8"
-    meta http-equiv="X-UA-Compatible" content="chrome=1"
-    title Sinatra Recipes
-    link rel="stylesheet" type="text/css" href="/stylesheets/styles.css"
-    link rel="shortcut icon" href="https://github.com/sinatra/resources/raw/master/logo/favicon.ico"
-    script src="/javascripts/scale.fix.js"
-    script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"
-  body
-    #wrapper
-      aside
-        header
-          img src="https://github.com/sinatra/resources/raw/master/logo/sinatra-classic-156.png"
-          h1 Sinatra Recipes
-          p Comunity contributed recipes and techniques
-          p.view
-            a href="http://github.com/sinatra/sinatra-recipes"
-              | View the Project on GitHub <small>sinatra/sinatra</small>
-          ul
-            li
-              a href="https://github.com/sinatra/sinatra-recipes/zipball/master"
-                | Download <strong>ZIP File</strong>
-            li
-              a href="https://github.com/sinatra/sinatra-recipes/tarall/master"
-                | Download <strong>TAR File</strong>
-            li
-              a href="https://github.com/sinatra/sinatra-recipes"
-                | Fork on <strong>GitHub</strong> 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Sinatra Recipes</title>
+    <link rel="stylesheet" type="text/css" href="/stylesheets/styles.css" /> 
+    <link rel="stylesheet" type="text/css" href="/stylesheets/pygment_trac.css" /> 
+    <link rel="shortcut icon" href="https://github.com/sinatra/resources/raw/master/logo/favicon.ico">
+    <script src="/javascripts/scale.fix.js"/>
+    <script type='text/javascript' 
+      src='https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'>
+    </script>
+  </head>
+  <body>
+    <div class="wrapper">
+    <aside>
+        <header>
+            <img src="https://github.com/sinatra/resources/raw/master/logo/sinatra-classic-156.png" />
+          <h1>
+            Sinatra Recipes
+          </h1>
+          <p>Community contributed recipes and techniques</p>
+          <p class="view"><a href="http://github.com/sinatra/sinatra-recipes">View the Project on GitHub <small>sinatra/sinatra-recipes</small></a></p>
+          <ul>
+            <li><a href="https://github.com/sinatra/sinatra-recipes/zipball/master">Download <strong>ZIP File</strong></a></li>
+            <li><a href="https://github.com/sinatra/sinatra-recipes/tarball/master">Download <strong>TAR Ball</strong></a></li>
+            <li><a href="http://github.com/sinatra/sinatra-recipes">Fork On <strong>GitHub</strong></a></li>
+          </ul>
+        </header>
+        <nav>
+          <h2>Topics</h2>
+          <dl>
+            <% @menu.each do |me| %>
+              <dt>
+                <a href='/p/<%= "#{me}" %>'>
+                  <%= me.capitalize.sub('_', ' ') %>
+                </a>
+              </dt>
+            <% end %>
+          </dl>
+        </nav>
+      </aside>
+      <section>
+        <div id="post"> 
+          <%= yield %>
+          <% if @children %>
+            <div id="children">
+              <ul>
+              <% @children.each do |child| %>
+                <li>
+                  <a href='/p/<%= "#{params[:topic]}/#{child}" %>'>
+                    <%= child.capitalize.sub('_', ' ') %>
+                  </a>
+                </li>
+              <% end %>
+              </ul>
+            </div>
+          <% end %>
 
-        nav
-          h2 Topics
-          dl
-            - @menu.each do |me|
-              dt
-                a href="/p/#{me}" #{me.capitalize.sub('_', ' ')}
-          p 
-            small Theme by <a href="https://github.com/orderedlist">orderedlist</a>
+          <% if @readme %>
+            <div id="footer">
+              <h2>Did we miss something?</h2>
+              <p>It's very possible we've left something out, thats why we need your help! This
+              is a community driven project after all. Feel free to fork the project and send
+              us a pull request to get your recipe or tutorial included in the book.</p>
+              <p>See the <a href="http://github.com/sinatra/recipes/blob/master/README.md">README</a> for more details.</p>
+            </div>
+          <% end %>
 
-      section
-        #post
-          == yield
-          - if @children
-            ul
-              - @children.each do |child|
-                li
-                  a href="/p/#{params[:topic]}/#{child}"
-                    == child.capitalize.sub('_', ' ')
+          <% if @contributors %>
+            <h2>Contributors</h2>
+            <p>These recipes are provided by the following outstanding members of the Sinatra community:</p>
+            <ul id="contributors">
+              <% @contributors.each do |contributor| %>
+                <li>
+                  <a href="http://github.com/<%= contributor["login"] %>">
+                    <img src="http://www.gravatar.com/avatar/<%= contributor["gravatar_id"] %>?s=50" />
+                  </a>
+                </li>
+              <% end %>
+            </ul>
+          <% end %>
+        </div>
+      </section>
+    
+      <footer>
+        <p><small>Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
+      </footer>
+      <a href="http://github.com/sinatra/sinatra-recipes">
+        <img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" />
+      </a>
+    </div>
+  </body>
+</html>
 
-          - if @readme
-            #footer
-              h2 Did we miss something?
-              p
-               | It's very possible we've left something out, that's why we need your help!
-               | This is a community driven project after all. Feel free to fork the project 
-               | and send us a pull request to get your recipe or tutorial included in the book. 
-              p 
-               | See the <a href="http://github.com/sinatra/recipes/blob/master/README.md">README</a> 
-               | for more details.
-          
-          - if @contributors
-            h2 Contributors
-            p 
-              | These recipes are provided by the following outsanding members of the Sinatra 
-              | community:
-            ul id="contributors"
-              - @contributors.each do |contributor|
-                li
-                  a href="http://github.com/#{contributor["login"]}"
-                    img src="http://www.gravatar.com/avatar/#{contributor["gravatar_id"]}?s=50"
-
-    a href="http://github.com/sinatra/sinatra-recipes"
-      img style="position: absolute;top:0;right:0;border:0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"
 
